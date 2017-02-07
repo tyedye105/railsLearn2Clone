a162273a0a1474698bd4c54a7b26338ba389ee96
Cody Brubaker and Marty Kovach <marty@>
2017-02-07 09:08:28 -0800

    add errors partial

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -5,4 +5,20 @@ class LessonsController < ApplicationController
   def show
     @lesson = Lesson.find(params[:id])
   end
+  def new
+    @lesson = Lesson.new
+  end
+  def create
+    @lesson = Lesson.new(lesson_params)
+    if @lesson.save
+      redirect_to lessons_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def lesson_params
+    params.require(:lesson).permit(:title, :content, :number)
+  end
 end
